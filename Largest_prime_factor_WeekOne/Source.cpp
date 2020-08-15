67bd59f00267c62cb28690abd0bf066b9ededb94
watcharapol yotadee <watcharapolyotadee@outlook.com>
2020-08-15 15:34:54 +0700

    whille

---
+++ Largest_prime_factor_WeekOne/Source.cpp
@@ -0,0 +1,24 @@
+﻿#include<stdio.h>
+#define number 600851475143
+int main()
+{
+	long long num = 0;
+	int maxPrime = 0, oodPrime = 3; // จำนวนเฉพาะ
+	num = number;
+	printf("num =  %d \n ", num);
+
+	while (num % 2 == 0)
+	{
+		maxPrime = 2;
+		num = num / 2;
+	}
+	printf("maxPrime = %d \t number /= %d  \n", maxPrime, number);
+	
+
+
+
+
+
+
+	return 0;
+}
